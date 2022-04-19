d2b381b3156b2dbc967d54d3e2384a0f279e6444
@darkcode01 <j0s3s3gur4@gmail.com>
2022-04-19 08:12:35 -0400

    feat: addd url for developer data on config

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,9 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Data for load
+config :initial_data_app, :initial_data, developers: System.get_env("DEVELOPERS_JSON_DATA")
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
